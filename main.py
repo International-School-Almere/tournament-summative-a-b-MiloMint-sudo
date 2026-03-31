d14b72d370c29614fc4432b7bf67310f2f51bc5d
MiloMint-sudo <miaterweijden@gmail.com>
2026-03-31 10:44:56 +0200

    Initial GUI setup: Defined main window, resolution, and page frames

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 #main file for the Tournament App.
+import tkinter as tk 
+import os 
+from tkinter import messagebox
+#-------------GUI-------------
+screen = tk.Tk()
+screen.title("Ron's College Tournament App")
+screen.geometry("1450x900")
+screen.minsize(800, 500)
+
+#-------------School logo / logo------------
+
+#-------------Pages-------------
+home_page = tk.Frame(screen)
+leaderboard_page = tk.Frame(screen)
+participant_page = tk.Frame(screen)
+sign_up_page = tk.Frame(screen)
+
+#-------------Buttons-------------
+
+
+screen.mainloop() 
