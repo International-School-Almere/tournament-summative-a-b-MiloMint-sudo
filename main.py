8bcbf23fea18c231d18628edb4517137bce8341f
MiloMint-sudo <miaterweijden@gmail.com>
2026-04-13 10:32:21 +0200

    Implement home page layout and sign-up functionality in the tournament app

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import tkinter as tk 
 import os 
 from tkinter import messagebox
+from sign_up_page import setup_signup_page
 #-------------GUI-------------
 screen = tk.Tk()
 screen.title("Ron's College Tournament App")
@@ -36,6 +37,36 @@ leaderboard_button.grid(row=0, column=2, padx=10, pady=10)
 participant_button.grid(row=0, column=3, padx=10, pady=10)
 sign_up_button.grid(row=0, column=4, padx=10, pady=10)
 
+container = tk.Frame(screen)
+container.configure(bg="white")
+container.grid(row=1, column=0, columnspan=6, sticky="nsew")
+
+def show_home_page():
+    for widget in container.winfo_children():
+        widget.destroy()
+    container.grid(row=1, column=0, columnspan=6, sticky="nsew")
+
+    school_name_label = tk.Label(container, text="Ron's College", font=("Helvetica", 16))
+    school_name_label.grid(pady=20, column=0, padx=25, sticky="nw")
+
+    Home_page_title = tk.Label(container, text="Ron's college 2026 Sports and Academic Tournament", font=("Helvetica", 20))
+    Home_page_title.grid(pady=20, column=1, padx=25, sticky="n")
+
+    rules_text_title = tk.Label(container, text="Tournament Rules", font=("Helvetica", 16))
+    rules_text_title.grid(row=1, column=2, pady=10, padx=10, sticky="w")
+
+    rules_text = "1. Respect all participants\n2. Follow the rules\n3. Have fun!"
+    rules_display = tk.Label(container, text=rules_text, font=("Helvetica", 13), justify="left", wraplength=200)
+    rules_display.grid(row=2, pady=5, column=2, padx=15, sticky="n")
+
+    game_play_title = tk.Label(container, text="How the games will work", font=("Helvetica", 16), wraplength=170)
+    game_play_title.grid(row=1, column=3, pady=10, padx=10, sticky="w")
+
+    game_play = "blah blah blah"
+    game_play_display = tk.Label(container, text=game_play, font=("Helvetica", 13), justify="left", wraplength=200)
+    game_play_display.grid(row=2, pady=5, column=3, padx=15, sticky="n")
+        
+
 def show_frame(frame):
     frame.tkraise()
     home_button.config(command=lambda: show_frame(home_page))
@@ -48,4 +79,7 @@ def show_frame(frame):
     tk.Label(participant_page, text="Participants").grid()
     tk.Label(sign_up_page, text="Sign Up").grid() 
 
+
+setup_signup_page(sign_up_page)
+show_home_page()
 screen.mainloop()
